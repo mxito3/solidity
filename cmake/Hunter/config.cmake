0db9c969cd48dff7382ff13a82d172c0242c7f10
mxito3 <1477311729@qq.com>
2019-03-04 14:41:43 +0800

    add hunter to manage packages

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,5 @@
+# 1.58.0 issue https://svn.boost.org/trac/boost/ticket/11410
+# 1.57.0-1.56.0 Doesn't work on Linux (GCC, c++11)
+
+hunter_config(Boost VERSION 1.65.1)
+# hunter_config(Sober VERSION ${HUNTER_Sober_VERSION} CMAKE_ARGS SOBER_RELAX_WARNINGS=ON)
